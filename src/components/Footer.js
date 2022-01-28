9629e010c8e04c3df3801a85e70dad52d7aaa018
Cloudy_minded <69113818+SupratimSarkar2001@users.noreply.github.com>
2022-01-28 12:12:32 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Modal from "./Modal";
+const Footer = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <>
+      <footer className="footer">
+        <button className="rules" onClick={toggle}>
+          Rules
+        </button>
+      </footer>
+      {modal ? <Modal toggle={toggle} /> : null}
+    </>
+  );
+};
+
+export default Footer;
